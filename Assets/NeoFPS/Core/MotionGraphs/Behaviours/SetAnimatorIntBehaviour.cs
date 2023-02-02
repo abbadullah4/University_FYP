b6bf68fe7def5116162300e772d8e54cd4f6ed15
SmilingDanger13 <abbadullah786@gmail.com>
2023-02-02 23:37:38 +0500

    NeoFPS Added...

---
+++ Assets/NeoFPS/Core/MotionGraphs/Behaviours/SetAnimatorIntBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using NeoFPS.CharacterMotion;
+using NeoFPS.CharacterMotion.Parameters;
+using NeoSaveGames.Serialization;
+using System.Collections;
+
+namespace NeoFPS.CharacterMotion
+{
+    [MotionGraphElement("Animation/SetAnimatorInt", "SetAnimatorIntBehaviour")]
+    public class SetAnimatorIntBehaviour : MotionGraphBehaviour
+    {
+        [SerializeField, Tooltip("The name of the animator parameter to write to.")]
+        private string m_ParameterName = string.Empty;
+        [SerializeField, Tooltip("When should the parameter be modified.")]
+        private When m_When = When.OnEnter;
+        [SerializeField, Tooltip("The value to write to the parameter on entering the state / subgraph.")]
+        private int m_OnEnterValue = 0;
+        [SerializeField, Tooltip("The value to write to the parameter on exiting the state / subgraph.")]
+        private int m_OnExitValue = 0;
+
+        private int m_ParameterHash = -1;
+
+        public enum When
+        {
+            OnEnter,
+            OnExit,
+            Both
+        }
+
+        public override void Initialise(MotionGraphConnectable o)
+        {
+            base.Initialise(o);
+
+            if (controller.bodyAnimator != null && !string.IsNullOrWhiteSpace(m_ParameterName))
+                m_ParameterHash = Animator.StringToHash(m_ParameterName);
+            else
+                enabled = false;
+        }
+
+        public override void OnEnter()
+        {
+            if (m_When != When.OnExit)
+                controller.bodyAnimator.SetInteger(m_ParameterHash, m_OnEnterValue);
+        }
+
+        public override void OnExit()
+        {
+            if (m_When != When.OnEnter)
+                controller.bodyAnimator.SetInteger(m_ParameterHash, m_OnExitValue);
+        }
+    }
+}
